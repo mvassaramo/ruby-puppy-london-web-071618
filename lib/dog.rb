fb729189bfd1ef93e8e4e475151c09b5338f9cfb
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-19 19:10:14 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+require 'pry' 
+
+
+class Dog 
+  
+  attr_reader :name 
+  
+ @@all = [] 
+ 
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.clear_all 
+    @@all = []
+  end 
+  
+  def self.all
+    @@all.each {|dog| puts dog.name }
+  end 
+  
+  
+end 
